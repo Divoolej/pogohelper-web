bb49a112a22e911b111d52727be3755d01f1cee1
Wojciech Olejnik <divoolej@gmail.com>
2018-12-18 21:54:29 +0100

    Add a deployment configuration

---
+++ shipitfile.js
@@ -0,0 +1,32 @@
+const shipItDeploy = require('shipit-deploy');
+
+module.exports = shipit => {
+  shipItDeploy(shipit);
+
+  shipit.initConfig({
+    default: {
+      branch: 'production',
+      dirToCopy: 'build',
+      deployTo: '/var/apps/pogohelper-web',
+      keepReleases: 2,
+      deleteOnRollback: false,
+      shallowClone: true,
+      repositoryUrl: 'https://github.com/Divoolej/pogohelper-web.git',
+    },
+    production: {
+      servers: 'deploy@pogohelper.top:2137',
+    },
+  });
+
+  shipit.on('fetched', () => {
+    shipit.start('build');
+  });
+
+  shipit.blTask('build', async () => {
+    await shipit.local(`
+      cd ${shipit.workspace} &&
+      yarn install &&
+      yarn build --environment production`
+    );
+  });
+}
