996783679d26f030f25ddb6a5ad7d0223dd94f78
Wojciech Olejnik <divoolej@gmail.com>
2018-12-20 17:17:31 +0100

    Add husky and sgvo, small fixes before deploy

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,23 +1,19 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 
 import Header from 'components/common/Header';
 import TypingsView from 'components/views/TypingsView';
 import EncountersView from 'components/views/EncountersView';
 
-const App = () => {
-  useEffect(() => console.log('App used effect'));
-
-  return (
-    <BrowserRouter>
-      <Fragment>
-        <Route exact path="/" render={() => <Redirect to="/types" />} />
-        <Route path="/" component={Header} />
-        <Route path="/types" component={TypingsView} />
-        <Route path="/encounters" component={EncountersView} />
-      </Fragment>
-    </BrowserRouter>
-  );
-};
+const App = () => (
+  <BrowserRouter>
+    <Fragment>
+      <Route exact path="/" render={() => <Redirect to="/types" />} />
+      <Route path="/" component={Header} />
+      <Route path="/types" component={TypingsView} />
+      <Route path="/encounters" component={EncountersView} />
+    </Fragment>
+  </BrowserRouter>
+);
 
 export default App;
